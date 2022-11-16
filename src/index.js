dcc14150c9dd58b31af0252d4c1ae710c854f08a
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-16 17:52:10 -0600

    commented three0

--- src/index.js
+++ src/index.js
@@ -5,22 +5,30 @@ import { init } from '@three0dev/js-sdk';
 import './App.css'
 import { env } from './env';
 import reportWebVitals from './reportWebVitals';
-
 import { StateProvider } from "./context/StateProvider";
 import reducer, { initialState } from "./context/Reducer";
 
-init(env.three0Config)
-  .then(() => {
-    const container = document.querySelector('#root')
-    const root = createRoot(container)
-    // eslint-disable-next-line react/jsx-filename-extension
-    root.render(
-      <StateProvider initialState={initialState} reducer={reducer}>
-        <App />
-      </StateProvider>
-    )
-  })
-  .catch(console.error)
+import ReactDOM from 'react-dom/client';
+// init(env.three0Config)
+//   .then(() => {
+//     const container = document.querySelector('#root')
+//     const root = createRoot(container)
+//     // eslint-disable-next-line react/jsx-filename-extension
+//     root.render(
+//       <StateProvider initialState={initialState} reducer={reducer}>
+//         <App />
+//       </StateProvider>
+//     )
+//   })
+//   .catch(console.error)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+  </StateProvider>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
