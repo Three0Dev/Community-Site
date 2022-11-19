aedf2d358177ff833b461cf4492e9d9ff1c6745a
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-19 10:06:24 -0600

    completed Search Bar

---
+++ src/pages/Header/SearchBar.jsx
@@ -0,0 +1,39 @@
+import "../../styles/Header/SearchBar.css"
+import React from "react";
+import { useEffect } from "react";
+
+function Search(){
+    const [inputValue, setInputValue] = React.useState('');
+    const search = async (e) =>{
+        // e.preventDefault();
+        alert("you searched", inputValue);
+    } 
+
+    useEffect(() =>{
+        if (inputValue){
+            // this is where we filter dater
+            console.log("filtering data", inputValue);
+        }
+
+
+    }, [inputValue])
+
+    return (
+    <div className="search-bar-container">
+        <div className="search-bar-wrapper">
+            <div className={inputValue=="" ? "mini-search-title" : "mini-search-title hidden" }>
+                <b>explore</b>
+            </div>
+            <div className="search-input-box">
+                <input autoFocus placeholder="three0" onChange={(e)=>
+                setInputValue(e.target.value)}>
+                </input>
+            </div>
+        </div>
+    </div>
+
+    )
+}
+
+
+export default Search
