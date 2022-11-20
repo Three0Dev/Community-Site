d735abfea398c4d3325f3b513851549ca837de05
Emil Velasquez <evelas10@utexas.edu>
2022-11-20 15:55:46 -0600

    Implemented Project Card

---
+++ src/pages/Body/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import "../../styles/Body/ProjectCard.css"
+
+import React, { useState } from "react";
+
+function ProjectCard({ title, creator, id, imgLink, tags }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
+      className={isHovered ? "project-card" : "project-card unfocused"}>
+      <div>
+        <img className="project-thumbnail" src={imgLink} alt="project thumbnail" />
+      </div>
+      {isHovered ?
+        <div className="project-info">
+          <div className="project-left-side">
+            <p className="project-title">{title}</p>
+            <p className="project-creator">{creator}</p>
+          </div>
+          <div className="project-id">
+            {id}
+          </div>
+        </div> :
+        <div className="project-tags">
+          {tags.map((tag) => tag + " ")}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default ProjectCard;
