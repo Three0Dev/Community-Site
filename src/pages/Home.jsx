0d847ae07dd5335a485d869ca4e72931bf333c6f
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-20 18:02:27 -0600

    added searchBar and changed background color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import "../styles/Home.css"
-
+import SearchBar from "./Header/SearchBar";
 import NavBar from "./Header/NavBar";
 
 function Home() {
   return (
     <div className="home">
       <NavBar />
+      <SearchBar />
     </div>
   )
 }
