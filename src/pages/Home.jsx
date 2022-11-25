7f45698acdc4d36d76961dbdd29b0f801a9d73ba
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-24 21:13:19 -0600

    imported projectCard and MainBody

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import "../styles/Home.css"
 import SearchBar from "./Header/SearchBar";
 import NavBar from "./Header/NavBar";
-
+import ProjectCard from "./Body/ProjectCard";
+import MainBody from "./Body/MainBody";
 function Home() {
   return (
     <div className="home">
