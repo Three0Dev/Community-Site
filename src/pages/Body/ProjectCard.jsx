17a35bb2f855db94b5decf0a0af978a1fbb6d5db
Emil Velasquez <evelas10@utexas.edu>
2022-11-30 21:23:48 -0600

    Project cards now clickable

--- src/pages/Body/ProjectCard.jsx
+++ src/pages/Body/ProjectCard.jsx
@@ -1,30 +1,33 @@
 import "../../styles/Body/ProjectCard.css"
 
 import React, { useState } from "react";
-// i think this needs to be link clickable
+import { Link } from "react-router-dom";
+
 function ProjectCard({ title, creator, id, imgLink, tags }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
       className={isHovered ? "project-card" : "project-card unfocused"}>
-      <div>
-        <img className="project-thumbnail" src={imgLink} alt="project thumbnail" />
-      </div>
-      {isHovered ?
-        <div className="project-info">
-          <div className="project-left-side">
-            <p className="project-title">{title}</p>
-            <p className="project-creator">{creator}</p>
-          </div>
-          <div className="project-id">
-            {id}
-          </div>
-        </div> :
-        <div className="project-tags">
-          {tags.map((tag) => tag + " ")}
+      <Link className="project-card-link" to={`/project/${id}`}>
+        <div>
+          <img className="project-thumbnail" src={imgLink} alt="project thumbnail" />
         </div>
-      }
+        {isHovered ?
+          <div className="project-info">
+            <div className="project-left-side">
+              <p className="project-title">{title}</p>
+              <p className="project-creator">{creator}</p>
+            </div>
+            <div className="project-id">
+              {id}
+            </div>
+          </div> :
+          <div className="project-tags">
+            {tags.map((tag) => tag + " ")}
+          </div>
+        }
+      </Link>
     </div>
   )
 }
---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+export async function projectLoader({ params }) {
+  return params.projectId;
+}
+
+function ProjectPage() {
+  const projectId = useLoaderData();
+
+  return (
+    <div>
+      Project ID: {projectId}
+    </div>
+  )
+}
+
+export default ProjectPage;
